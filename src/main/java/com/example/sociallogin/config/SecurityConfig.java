210da1a0a5f4307376852acbd1f25f08fd38976d
Darshita <108191705+darshita2000@users.noreply.github.com>
2024-04-22 11:54:51 +0530

    first commit

---
+++ src/main/java/com/example/sociallogin/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.sociallogin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static javax.management.Query.and;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .oauth2Login(withDefaults())
+                .build();
+    }
+}
